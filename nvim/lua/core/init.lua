5fa6b47932354474ae98551387e4c1c84f5a85d5
WoodHolz <1535980162@qq.com>
2023-08-15 20:44:41 +0800

    update

---
+++ nvim/lua/core/init.lua
@@ -0,0 +1,167 @@
+local opt = vim.opt
+local g = vim.g
+local config = require("core.utils").load_config()
+
+-------------------------------------- globals -----------------------------------------
+g.nvchad_theme = config.ui.theme
+g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+g.toggle_theme_icon = "   "
+g.transparency = config.ui.transparency
+
+-------------------------------------- options ------------------------------------------
+local my_auto_save = true
+local my_auto_restore_cursor_position = true
+local my_auto_remove_new_lines_comment = true
+
+opt.laststatus = 3 -- global statusline
+opt.showmode = false
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 2
+opt.ruler = false
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+
+g.mapleader = " "
+
+-- disable some default providers
+for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
+  vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+
+-------------------------------------- autocmds ------------------------------------------
+local autocmd = vim.api.nvim_create_autocmd
+
+-- dont list quickfix buffers
+autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.opt_local.buflisted = false
+  end,
+})
+
+-- reload some chadrc options on-save
+autocmd("BufWritePost", {
+  pattern = vim.tbl_map(function(path)
+    return vim.fs.normalize(vim.loop.fs_realpath(path))
+  end, vim.fn.glob(vim.fn.stdpath "config" .. "/lua/custom/**/*.lua", true, true, true)),
+  group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
+
+  callback = function(opts)
+    local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
+    local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
+    local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
+
+    require("plenary.reload").reload_module "base46"
+    require("plenary.reload").reload_module(module)
+    require("plenary.reload").reload_module "custom.chadrc"
+
+    config = require("core.utils").load_config()
+
+    vim.g.nvchad_theme = config.ui.theme
+    vim.g.transparency = config.ui.transparency
+
+    -- statusline
+    require("plenary.reload").reload_module("nvchad.statusline." .. config.ui.statusline.theme)
+    vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.ui.statusline.theme .. "').run()"
+
+    -- tabufline
+    if config.ui.tabufline.enabled then
+      require("plenary.reload").reload_module "nvchad.tabufline.modules"
+      vim.opt.tabline = "%!v:lua.require('nvchad.tabufline.modules').run()"
+    end
+
+    require("base46").load_all_highlights()
+    -- vim.cmd("redraw!")
+  end,
+})
+
+-------------------------------------- commands ------------------------------------------
+local new_cmd = vim.api.nvim_create_user_command
+
+new_cmd("NvChadUpdate", function()
+  require "nvchad.updater"()
+end, {})
+
+-------------------------------------- my_autocmds ---------------------------------------
+local vim = vim
+
+-- auto save buffer
+if my_auto_save then
+    vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+        pattern = { "*" },
+        callback = function()
+            local disable_file_types = {
+                "",
+                "toggleterm",
+                "translate",
+            }
+            local directory = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:h")
+            if vim.fn.isdirectory(directory) == 0 and not vim.tbl_contains(disable_file_types, vim.bo.filetype) then
+                vim.fn.mkdir(directory, "p")
+            end
+            vim.cmd("silent! wall")
+        end,
+        nested = true,
+    })
+end
+
+-- auto restore cursor position
+if my_auto_restore_cursor_position then
+    vim.api.nvim_create_autocmd("BufReadPost", {
+        pattern = { "*" },
+        callback = function()
+            if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+                vim.fn.setpos(".", vim.fn.getpos("'\""))
+                -- how do I center the buffer in a sane way??
+                -- vim.cmd('normal zz')
+                vim.cmd("silent! foldopen")
+            end
+        end,
+    })
+end
+
+-- remove auto-comments
+if my_auto_remove_new_lines_comment then
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+        pattern = { "*" },
+        callback = function()
+            vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+        end,
+    })
+end
