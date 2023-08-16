876d532e6e8733a51f39c71ea342793f4a2b2960
WoodHolz <1535980162@qq.com>
2023-08-16 11:56:03 +0800

    update

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "github_dark",
-  theme_toggle = { "github_dark", "github_dark" },
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
